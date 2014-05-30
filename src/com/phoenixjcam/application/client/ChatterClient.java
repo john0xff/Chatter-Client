68228ef3da56e174a6df9c5d8857b291cdc202c6
BartBien <phoenixjcam@gmail.com>
2014-05-30 18:09:50 +0200

    added time to messages

--- src/com/phoenixjcam/application/client/ChatterClient.java
+++ src/com/phoenixjcam/application/client/ChatterClient.java
@@ -10,180 +10,168 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class ChatterClient extends JFrame
-{
+public class ChatterClient extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private final int width = 640;
-	private final int height = 480;
+    private static final long serialVersionUID = 1L;
+    private final int width = 640;
+    private final int height = 480;
 
-	// connection
-	private String serverIP;
-	private int port;
-	private Socket connectionSocket;
+    // connection
+    private String serverIP;
+    private int port;
+    private Socket connectionSocket;
 
-	// streams
-	private ObjectOutputStream output;
-	private ObjectInputStream input;
+    // streams
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
 
-	// components
-	private JScrollPane scrollPane;
-	private JTextArea chatArea;
-	private JTextField userText;
+    // components
+    private JScrollPane scrollPane;
+    private JTextArea chatArea;
+    private JTextField userText;
 
-	private String message;
-	private final static String NEWLINE = "\n";
-	/** exit command */
-	private final static String EXITCMD = "\nSERVER - EXIT";
+    private String message;
+    private final static String NEWLINE = "\n";
+    /** exit command */
+    private final static String EXITCMD = "\nSERVER - EXIT";
 
-	public ChatterClient(String serverIP, int port)
-	{
-		super("Chatter Client");
+    // time
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private String currentTime;
 
-		this.serverIP = serverIP;
-		this.port = port;
+    public ChatterClient(String serverIP, int port) {
+	super("Chatter Client");
 
-		scrollPane = new JScrollPane(getChatArea());
-		add(scrollPane, BorderLayout.CENTER);
+	this.serverIP = serverIP;
+	this.port = port;
 
-		add(getUserText(), BorderLayout.NORTH);
+	dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-		frameSettings();
-		runClient();
-	}
+	scrollPane = new JScrollPane(getChatArea());
+	add(scrollPane, BorderLayout.CENTER);
 
-	private void frameSettings()
-	{
-		setSize(width, height);
-		Point centerPoint = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
-		setLocation(900, (centerPoint.y) - (height / 2));
+	add(getUserText(), BorderLayout.NORTH);
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-	}
+	frameSettings();
+	runClient();
+    }
 
-	private JTextArea getChatArea()
-	{
-		chatArea = new JTextArea();
-		chatArea.setFont(new Font("Arial", 0, 20));
-		return chatArea;
-	}
+    private void frameSettings() {
+	setSize(width, height);
+	Point centerPoint = GraphicsEnvironment.getLocalGraphicsEnvironment()
+		.getCenterPoint();
+	setLocation(900, (centerPoint.y) - (height / 2));
 
-	/** send client text to server and append client text to client chat area */
-	private JTextField getUserText()
-	{
-		userText = new JTextField();
-		userText.setFont(new Font("Arial", 0, 20));
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setVisible(true);
+    }
 
-		userText.addActionListener(new ActionListener()
-		{
+    private JTextArea getChatArea() {
+	chatArea = new JTextArea();
+	chatArea.setFont(new Font("Arial", 0, 20));
+	return chatArea;
+    }
 
-			@Override
-			public void actionPerformed(ActionEvent event)
-			{
-				String serverMessage = event.getActionCommand();
+    /** send client text to server and append client text to client chat area */
+    private JTextField getUserText() {
+	userText = new JTextField();
+	userText.setFont(new Font("Arial", 0, 20));
 
-				try
-				{
-					// send client text to server side
-					output.writeObject(NEWLINE + "CLIENT - " + serverMessage);
-					output.flush();
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
+	userText.addActionListener(new ActionListener() {
 
-				// append client text to area
-				chatArea.append(NEWLINE + "CLIENT - " + serverMessage);
+	    @Override
+	    public void actionPerformed(ActionEvent event) {
+		String serverMessage = event.getActionCommand();
 
-				userText.setText("");
-			}
-		});
+		try {
+		    calendar = Calendar.getInstance();
+		    currentTime = dateFormat.format(calendar.getTime());
 
-		return userText;
-	}
-
-	public void runClient()
-	{
-
-		chatArea.append("Attempting connection" + NEWLINE);
-
-		try
-		{
-			// InetAddress address = InetAddress.getByName(serverIP);
-			connectionSocket = new Socket(serverIP, port);
-
-			chatArea.append("connected to " + serverIP + NEWLINE);
-
-			output = new ObjectOutputStream(connectionSocket.getOutputStream());
-			output.flush();
-			input = new ObjectInputStream(connectionSocket.getInputStream());
-
-			do
-			{
-				message = (String) input.readObject();
-
-				chatArea.append(message);
-			}
-			while (!message.equals(EXITCMD));
-
-			closeStreams();
-			closeSocket();
-			// ? dispose();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		catch (ClassNotFoundException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		    // send client text to server side
+		    output.writeObject(NEWLINE + currentTime + " CLIENT - "
+			    + serverMessage);
+		    output.flush();
+		} catch (IOException e) {
+		    e.printStackTrace();
 		}
 
-		String hostName = connectionSocket.getInetAddress().getHostName();
-		chatArea.setText("connected to " + hostName + NEWLINE);
+		// append client text to area
+		chatArea.append(NEWLINE + currentTime + " CLIENT - "
+			+ serverMessage);
+
+		userText.setText("");
+	    }
+	});
+
+	return userText;
+    }
+
+    public void runClient() {
+
+	chatArea.append("Attempting connection" + NEWLINE);
+
+	try {
+	    // InetAddress address = InetAddress.getByName(serverIP);
+	    connectionSocket = new Socket(serverIP, port);
+
+	    chatArea.append("connected to " + serverIP + NEWLINE);
+
+	    output = new ObjectOutputStream(connectionSocket.getOutputStream());
+	    output.flush();
+	    input = new ObjectInputStream(connectionSocket.getInputStream());
+
+	    do {
+		message = (String) input.readObject();
+
+		chatArea.append(message);
+	    } while (!message.equals(EXITCMD));
+
+	    closeStreams();
+	    closeSocket();
+	    // ? dispose();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	} catch (ClassNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
 
-	private void closeStreams()
-	{
-		try
-		{
-			output.close();
-			input.close();
-		}
-		catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	String hostName = connectionSocket.getInetAddress().getHostName();
+	chatArea.setText("connected to " + hostName + NEWLINE);
+    }
 
+    private void closeStreams() {
+	try {
+	    output.close();
+	    input.close();
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
 
-	private void closeSocket()
-	{
-		try
-		{
-			connectionSocket.close();
-		}
-		catch (IOException ioException)
-		{
-			ioException.printStackTrace();
-		}
-	}
+    }
 
-	public static void main(String[] args)
-	{
-		String serverIP = "127.0.0.1";
-		int port = 9002;
-
-		new ChatterClient(serverIP, port);
+    private void closeSocket() {
+	try {
+	    connectionSocket.close();
+	} catch (IOException ioException) {
+	    ioException.printStackTrace();
 	}
+    }
+
+    public static void main(String[] args) {
+	String serverIP = "127.0.0.1";
+	int port = 9002;
+
+	new ChatterClient(serverIP, port);
+    }
 }
